3d762b30be65e7bbbd0be401182f5b7f6e3fcfe0
Gustavo Silva <gustavo.silva.nav@gmail.com>
2022-08-16 14:05:16 -0300

    feat(express): setting up express server and session
    Setting up the express server, cookies, sessions and passport, passport still half way, still need
    to work on it

--- src/app.js
+++ src/app.js
@@ -1,5 +1,41 @@
-const main = (value) => {
-  console.log(value);
-};
+import 'dotenv/config';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
+import MongoStore from 'connect-mongo';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import morgan from 'morgan';
 
-main('Hello World');
+import env from './utils/variables-env.js';
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.set('port', process.env.PORT || 8080);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    store: MongoStore.create({ mongoUrl: env.dbName }),
+    secret: env.secretKey,
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+      maxAge: 60000,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(morgan('dev'));
+
+app.listen(app.get('port'), () => {
+  console.log('Server on Port: ', app.get('port'));
+});
---
+++ src/utils/variables-env.js
@@ -0,0 +1,4 @@
+export default {
+  dbName: process.env.MDB_DATABASE_CONNECTION,
+  secretKey: process.env.SESSION_KEY_CODE,
+};
