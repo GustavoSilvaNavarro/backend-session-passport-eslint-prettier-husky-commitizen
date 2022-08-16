356654b6aa4dc4618531da2b5bfec66653497c0c
Gustavo Silva <gustavo.silva.nav@gmail.com>
2022-08-16 16:51:54 -0300

    feat(handlebars): configuration to hbs, userRoutes
    Setting up of the handlebars, and added the handelbars html to make the app work,  user Routes set
    up!!!

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import passport from 'passport';
 import MongoStore from 'connect-mongo';
+import { engine } from 'express-handlebars';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import morgan from 'morgan';
@@ -16,6 +17,17 @@ connectDB();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+  '.hbs',
+  engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+  })
+);
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -37,6 +49,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(morgan('dev'));
+import userRoutes from './routes/users-routes.js';
+
+app.use('/', userRoutes);
 
 app.listen(app.get('port'), () => {
   console.log('Server on Port: ', app.get('port'));
