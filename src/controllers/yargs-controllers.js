7cf7876d1082fc768357923ab68d22b8d2d9aa6f
Gustavo Antonio Silva Navarro <gustavo.silva.nav@gmail.com>
2022-08-22 13:08:12 -0300

    feat(inforoute): route was implemented using process to get info of the project
    I setup the route info with the info of the project and pc using the comando process, native in
    nodejs

--- src/controllers/yargs-controllers.js
+++ src/controllers/yargs-controllers.js
@@ -1,4 +1,35 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+//YARGS SETUP
+const args = yargs(hideBin(process.argv))
+  .alias({ p: 'port', n: 'name' })
+  .default({ port: 8080, name: 'Antonio' }).argv;
+
 //GET INFO OF THE PC WITH YARGS
-export const getInfoFromPC = (req, res) => {
-  res.send('Hello World');
+export const getInfoFromPC = (req, res, next) => {
+  try {
+    const entryArguments = `Port: ${args.port} - Name: ${args.name}`;
+    const result = {
+      entryArgs: entryArguments,
+      plataform: process.platform,
+      nodeVersion: process.version,
+      memoryUsage: process.memoryUsage().rss,
+      path: process.execPath,
+      processId: process.pid,
+      dirWorkName: process.cwd(),
+    };
+    res.status(200).render('info', { result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+//COMPUTE RANDOM NUMBERS USING FORK AND PROCESS
+export const computeRandomNumbers = (req, res, next) => {
+  try {
+    res.send('Hello World');
+  } catch (err) {
+    next(err);
+  }
 };
--- src/routes/yargs-routes.js
+++ src/routes/yargs-routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getInfoFromPC } from '../controllers/yargs-controllers.js';
+import {
+  getInfoFromPC,
+  computeRandomNumbers,
+} from '../controllers/yargs-controllers.js';
 
 //GET INFO OF THE PC WITH YARGS
 router.get('/info', getInfoFromPC);
+
+//COMPUTE RANDOM NUMBERS USING FORK AND PROCESS
+router.get('/api/randoms', computeRandomNumbers);
 
 export default router;
